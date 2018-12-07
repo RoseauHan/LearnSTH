c09830d277bd158a4a1e941ccf5b0c96cf00e636
roseauhan <roseauhan@gmail.com>
2018-12-07 15:32:20 +0800

    add string

---
+++ leetcode/string/validPalindrome.cpp
@@ -0,0 +1,18 @@
+//Valid Palindrome
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+		string s="roseauHan";
+		transform(s.begin(), s.end(), s.begin(), ::tolower);
+		auto left =s.begin(), right = prev(s.end());
+
+		while (left < right){
+				if (!::isalnum(*left)) ++left;
+				else if (!::isalnum(*right)) --right;
+				else if (*left != *right) return false;
+				else {left ++, right--;}
+		}
+		return true;
+}
+
